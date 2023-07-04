73982b24138c277cf0d802cb44ca191f4e7b5919
Yashua Pervez <yashuapervez@gmail.com>
2023-07-05 00:23:29 +0500

    feat: add SEO on page template

--- src/pages/page/[slug].tsx
+++ src/pages/page/[slug].tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
+// Components
+import SEOYoast from "@/common/components/SEOYoast";
+
 // Containers
 import Title from "@/common/containers/Title";
 import Content from "@/containers/SinglePage/Content";
@@ -10,10 +13,18 @@ import Wordpress from "@/services/Wordpress";
 
 type SinglePageProps = {
   page: any;
+  layoutData: any;
 };
-const SinglePage: NextPage<SinglePageProps> = ({ page }) => {
+const SinglePage: NextPage<SinglePageProps> = ({ page, layoutData }) => {
+  const { site_icon } = layoutData.siteData;
+  page.yoast_head_json.favIcon = site_icon.src;
+
   return (
     <>
+      <SEOYoast
+        yoast_head_json={page.yoast_head_json}
+        pagePath={`/${page.slug}`}
+      />
       <Title size="extra-small" name={page.title.rendered} />
       <Content page={page} />
     </>
