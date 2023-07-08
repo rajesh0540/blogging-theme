9f7e4e86afddff0dabafcf6461f352bf0861781d
Yashua Pervez <yashuapervez@gmail.com>
2023-07-08 12:54:06 +0500

    feat: add scroll bar to web stories component

--- src/common/layout/WebStories/Component.tsx
+++ src/common/layout/WebStories/Component.tsx
@@ -14,36 +14,42 @@ const WebStoriesComponent: React.FC<WebStoriesProps> = ({ webStories }) => {
     <>
       <div className="mb-5">
         <Wrapper>
-          <ul className="flex flex-wrap justify-center gap-4">
-            {webStories.map((story: any, index: number) => (
-              <li
-                className="p-[3px] overflow-hidden border-gray-500 rounded-full border-[2px] relative"
-                key={story.slug}
-              >
-                <div className="w-16 h-16 overflow-hidden rounded-full">
-                  {story.poster ? (
-                    <NextImage
-                      alt={`Story ${story.slug}`}
-                      className="object-cover w-full h-full"
-                      src={story.poster.url}
-                      height={(story.poster.height / story.poster.width) * 64}
-                      width={64}
-                      blurDataURL={story.poster.placeholder}
-                      placeholder={"blur"}
-                    />
-                  ) : (
-                    <div className="w-full h-full bg-gray-400"></div>
-                  )}
-                </div>
-                <Link
-                  href={`/web-stories/${story.slug}`}
-                  aria-label={`Story ${story.slug}`}
-                >
-                  <div className="absolute top-0 left-0 w-full h-full bg-gray-500 opacity-0 cursor-pointer hover:opacity-30"></div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <div className="max-w-[300px] lg:max-w-[500px] relative overflow-x-auto h-24 m-auto">
+            <div className="absolute top-0 left-0 min-w-[300px] lg:min-w-[500px]">
+              <ul className="flex justify-center gap-2">
+                {webStories.map((story: any, index: number) => (
+                  <li
+                    className="p-[3px] overflow-hidden border-gray-500 rounded-full border-[2px] relative"
+                    key={story.slug}
+                  >
+                    <div className="w-16 h-16 overflow-hidden rounded-full">
+                      {story.poster ? (
+                        <NextImage
+                          alt={`Story ${story.slug}`}
+                          className="object-cover w-full h-full"
+                          src={story.poster.url}
+                          height={
+                            (story.poster.height / story.poster.width) * 64
+                          }
+                          width={64}
+                          blurDataURL={story.poster.placeholder}
+                          placeholder={"blur"}
+                        />
+                      ) : (
+                        <div className="w-full h-full bg-gray-400"></div>
+                      )}
+                    </div>
+                    <Link
+                      href={`/web-stories/${story.slug}`}
+                      aria-label={`Story ${story.slug}`}
+                    >
+                      <div className="absolute top-0 left-0 w-full h-full bg-gray-500 opacity-0 cursor-pointer hover:opacity-30"></div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </Wrapper>
       </div>
     </>
